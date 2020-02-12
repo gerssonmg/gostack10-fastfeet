b03f9510e4dcc3224e7e7dd8625c3c46cacbed7f
Gerson <gersoncafilho@gmail.com>
2020-02-12 14:22:09 -0300

    struct basic Deliveryman

--- FastFeet/FastFeet-Backend/src/routes.js
+++ FastFeet/FastFeet-Backend/src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import DeliveryController from './app/controllers/DeliverymanController';
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
 
@@ -8,12 +9,16 @@ import AuthMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-// routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(AuthMiddleware);
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 routes.put('/users', UserController.update);
+
+routes.get('/deliveryman', DeliveryController.index);
+routes.post('/deliveryman', DeliveryController.store);
+routes.put('/deliveryman/:id', DeliveryController.update);
+routes.delete('/deliveryman', DeliveryController.delete);
 
 export default routes;
