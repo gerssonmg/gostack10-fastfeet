8814a400698db96d8615de69b62ef3e1c56e19e9
Gerson <gersoncafilho@gmail.com>
2020-02-12 17:24:19 -0300

    rename deliverys for correct 'deliveries'

--- FastFeet/FastFeet-Backend/src/routes.js
+++ FastFeet/FastFeet-Backend/src/routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
-import DeliveryController from './app/controllers/DeliverymanController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 import RecipientController from './app/controllers/RecipientController';
 import SessionController from './app/controllers/SessionController';
-import OrderController from './app/controllers/OrderController';
+import DeliveryController from './app/controllers/DeliveryController';
 
 import AuthMiddleware from './app/middlewares/auth';
 
@@ -17,14 +17,14 @@ routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
 routes.put('/users', UserController.update);
 
-routes.get('/deliveryman', DeliveryController.index);
-routes.post('/deliveryman', DeliveryController.store);
-routes.put('/deliveryman/:id', DeliveryController.update);
-routes.delete('/deliveryman/:id', DeliveryController.delete);
+routes.get('/deliveryman', DeliverymanController.index);
+routes.post('/deliveryman', DeliverymanController.store);
+routes.put('/deliveryman/:id', DeliverymanController.update);
+routes.delete('/deliveryman/:id', DeliverymanController.delete);
 
-routes.get('/order', OrderController.index);
-routes.post('/order', OrderController.store);
-routes.put('/order/:id', OrderController.update);
-routes.delete('/order/:id', OrderController.delete);
+routes.get('/delivery', DeliveryController.index);
+routes.post('/delivery', DeliveryController.store);
+routes.put('/delivery/:id', DeliveryController.update);
+routes.delete('/delivery/:id', DeliveryController.delete);
 
 export default routes;
